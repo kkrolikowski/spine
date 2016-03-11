44bfa48dc27ae1e287a416a3bf626f3fc318123c
Krzysztof Królikowski <kkrolikowski@gmail.com>
2016-03-11 14:16:18 +0100

    fork

--- spine-agent/src/main.c
+++ spine-agent/src/main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-int main(void) {
-	printf("Hello World!\n");
+int main(int argc, char *argv[]) {
+	pid_t parent, agentp, recvp, sendp, sid;
 
+	if((parent = fork()) < 0) {
+		fprintf(stderr, "[CRIT] Nie moge stworzyc procesu\n");
+		exit(EXIT_FAILURE);
+	}
+	if(parent > 0)
+		exit(EXIT_SUCCESS);
+
+	// demonizujemy proces
+	umask(0);
+	sid = setsid();
+	if(sid < 0)
+		exit(EXIT_FAILURE);
+	if((chdir("/")) < 0)
+		exit(EXIT_FAILURE);
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	// uruchamiamy proces nadrzedny
+	agentp = fork();
+	if(agentp > 0) {
+		recvp = fork();
+		if(!recvp)
+			while(1)
+				sleep(1);
+		sendp = fork();
+		if(!sendp)
+			while(1)
+				sleep(1);
+	}
+	else if(!agentp)
+		while(1)
+			sleep(1);
 	return 0;
 }
