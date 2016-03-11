a88e50f974da133415d42b15c14396e5097c4350
Krzysztof Królikowski <kkrolikowski@gmail.com>
2016-03-11 20:32:01 +0100

    ubijanie procesów
    po wysłaniu sygnału SIGTERM ubijamy workery i kończymy działanie programu

--- spine-agent/src/main.c
+++ spine-agent/src/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
+pid_t recvp = -1;
+pid_t sendp = -1;
+pid_t agentp = -1;
+
+void kill_workers(int sig);
+
 int main(int argc, char *argv[]) {
-	pid_t parent, agentp, recvp, sendp, sid;
+	pid_t parent, sid;
 
 	if((parent = fork()) < 0) {
 		fprintf(stderr, "[CRIT] Nie moge stworzyc procesu\n");
@@ -24,20 +32,36 @@ int main(int argc, char *argv[]) {
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
-
+	
 	// uruchamiamy proces nadrzedny
 	agentp = fork();
 	if(!agentp) {
+		// odpalamy proces odbierajacy dane
 		recvp = fork();
 		if(!recvp)
 			while(1)
 				sleep(1);
-		sendp = fork();
-		if(!sendp)
-			while(1)
-				sleep(1);
-		while(1)
-			sleep(1);
+		else if(recvp > 0) {
+			agentp = getppid();
+			// odpalamy proces wysylajacy dane
+			sendp = fork();
+			if(!sendp) {
+				while(1)
+					sleep(1);
+			}
+			while(1) {
+				if(signal(SIGTERM, kill_workers)) {
+					wait(NULL);
+					break;
+				}
+				else
+					sleep(1);
+			}
+		}
 	}
 	return 0;
+}
+void kill_workers(int sig) {
+	kill(recvp, SIGTERM);
+	kill(sendp, SIGTERM);
 }
