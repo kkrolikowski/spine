095f6d479427d12ff5e9b4392fdb59c199437294
Krzysztof Królikowski <kkrolikowski@gmail.com>
2016-03-21 21:59:03 +0100

    cleanup

--- spine-web/static/diskChart.js
+++ spine-web/static/diskChart.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
     url: "/hostinfo.php?uptime",
     method: "GET",
     success: function(response) {
-      var data = {response};
+      var data = response;
       Chart.defaults.global.legend = {
         enabled: false
       };
       var canvasDoughnut = new Chart(document.getElementById("canvas1"), {
         type: 'doughnut',
         tooltipFillColor: "rgba(51, 51, 51, 0.55)",
-        data: data
+        data: response
       });
 
     }
