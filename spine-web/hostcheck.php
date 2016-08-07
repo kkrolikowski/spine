aab8dd62505abf3c9632badbbcd9bf578f7ecf0d
Krzysztof Królikowski <kkrolikowski@gmail.com>
2016-08-07 13:45:57 +0200

    dodatkowa kolumna - status hosta
    U - Unavailable
    A - Available

--- spine-web/hostcheck.php
+++ spine-web/hostcheck.php
@@ -9,8 +9,15 @@
   $q->execute();
 
   while ($r = $q->fetch()) {
-    if(($now - $r['seen']) > 5)
+    if(($now - $r['seen']) > 10) {
       array_push($dead, $r['hostname']);
+      $q2 = $dbh->prepare("UPDATE sysinfo SET host_status = 'U' WHERE hostname = '".$r['hostname']."'");
+      $q2->execute();
+    }
+    else {
+      $q2 = $dbh->prepare("UPDATE sysinfo SET host_status = 'A' WHERE hostname = '".$r['hostname']."'");
+      $q2->execute();
+    }
   }
   if(count($dead) > 0) {
     header('Content-Type: application/json');
