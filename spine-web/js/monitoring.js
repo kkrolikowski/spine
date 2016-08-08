fc8dd9d485c614a05b215432e70c7038290c2bf9
Krzysztof Królikowski <kkrolikowski@gmail.com>
2016-08-08 10:41:16 +0200

    wcięcia

--- spine-web/js/monitoring.js
+++ spine-web/js/monitoring.js
@@ -7,17 +7,17 @@ function watch() {
           var json = {};
           json = JSON.parse(event.data);
           for(var i = 0; i < json.length; i++) {
-              alertify.error("HOST: " + json[i] + " is down!");
-              $('#alertBox-body > .list-group').append(
-                '<a href="#" class="list-group-item">' +
-                  '<i class="fa fa-warning fa-fw"></i> Host: ' + json[i] + ' is down.' +
-                  '<span class="pull-right text-muted small"><em>4 minutes ago</em></span>' +
-                '</a>'
-              );
-              $(".hostList:has(strong:contains('"+ json[i] +"'))")
-                .find('.label-success').removeClass('label-success')
-                .addClass('label-danger')
-                .html('Offline');
+            alertify.error("HOST: " + json[i] + " is down!");
+            $('#alertBox-body > .list-group').append(
+              '<a href="#" class="list-group-item">' +
+                '<i class="fa fa-warning fa-fw"></i> Host: ' + json[i] + ' is down.' +
+                '<span class="pull-right text-muted small"><em>4 minutes ago</em></span>' +
+              '</a>'
+            );
+            $(".hostList:has(strong:contains('"+ json[i] +"'))")
+              .find('.label-success').removeClass('label-success')
+              .addClass('label-danger')
+              .html('Offline');
           }
         }
     }
