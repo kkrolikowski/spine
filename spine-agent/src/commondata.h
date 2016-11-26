4a7976bafc7dd32141cb5ca27d83ccde16549c55
kkrolikowski <kkrolikowski@gmail.com>
2016-11-26 23:04:43 +0100

    redesign głównych struktur danych
    plik nagłówkowy podzieliłem na sekcje aby zwiększyć jego czytelność

--- spine-agent/src/commondata.h
+++ spine-agent/src/commondata.h
@@ -1,29 +1,9 @@
 #ifndef SPINE_AGENT_SRC_COMMONDATA_H_
 #define SPINE_AGENT_SRC_COMMONDATA_H_
 
-#define VHOST_MAX 200		// limit vhostow na serwerze
-
-/*			DANE			*/
-// konfiguracja serwera www
-typedef struct wwwdata {
-	char * ServerName;				// glowny adres witryny
-	char * ServerAlias;				// Dodatkowe adresy witryny
-	char * DocumentRoot;			// katalog witryny na serwerze
-	char * apacheOpts;				// opcje katalogu
-	char * htaccess;				// definicja pliku htaccess
-	char * vhost_access_order;		// kolejnosc przetwarzania regol dostepu
-	char * vhost_access_list;		// lista hostow skad mozna laczyc sie z witryna
-	int password_access;			// czy witryna ma byc zabezpieczona haslem
-	char * htusers;					// lista kont, ktora ma dostep do witryny
-	char * user;					// wlasciciel witryny
-	char * status;					// flaga: A - vhost aktywny, D - do skasowania
-	char * purgedir;					// flaga: Y - kasujemy pliki aplikacji, N - zostawiamy
-} wwwdata;
-
-typedef struct htpasswdData {
-	char * entry;
-	struct htpasswdData * next;
-} htpasswdData;
+/****************************************************/
+/*              Sekcja kont systemowych             */
+/****************************************************/
 
 // struktura przechowujaca klucze ssh uzytkownika
 typedef struct sshkeys {
@@ -44,7 +24,15 @@ typedef struct sysuser {
     struct sysuser * next;  // wskaznik do kolejnego konta
 } sysuser;
 
-// nowe struktury danych opisujace konfiguracje serwera www
+/****************************************************/
+/*              Sekcja konfiguracji serwera www     */
+/****************************************************/
+
+// lista zawierajaca konta htpasswd
+typedef struct htpasswdData {
+	char * entry;                   // wpis w formie: login:skrothasla 
+	struct htpasswdData * next;     // nastepne konto na liscie
+} htpasswdData;
 
 // konfiguracja vhostow
 typedef struct vhostdata {
@@ -63,22 +51,21 @@ typedef struct vhostdata {
     struct vhostdata * next;    // wskaznik do kolejnego vhosta
 } vhostdata;
 
+// ogolna struktura danych przechowujaca konfiguracje serwera www
 typedef struct httpdata {
     vhostdata * vhost;          // vhosty apacza
     htpasswdData * htpasswd;    // konta htpasswd
 } httpdata;
 
-// struktura, ktora bedzie przechowywac wskazniki
-// do poszczegolnych elementow pakietu.
+/****************************************************/
+/*          Ogolna konfiguracja systemu             */
+/****************************************************/
+
+// struktura przechowujaca konfiguracje hosta
 typedef struct hostconfig {
-    struct wwwdata vhost[VHOST_MAX];    // definicja vhostow
-    int vhost_num;                      // liczba skonfigurowanych vhostow
-    htpasswdData * htpasswd;            // zawartosc pliku htpasswd
     sysuser * sysUsers;                 // konta uzytkownikow w systemie
     httpdata httpd;                     // konfiguracja serwera www
-    int htusers_count;                  // liczba kont htpasswd
     int confVer;                        // wersja konfiguracji
-    char * datatype;                    // typ konfiguracji
 } hostconfig;
 
 #endif /* SPINE_AGENT_SRC_COMMONDATA_H_ */
