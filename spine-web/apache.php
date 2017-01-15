56e13dbc27499dd15fff9deeb5082435982134b2
Krzysztof Królikowski <kkrolikowski@gmail.com>
2017-01-15 01:32:09 +0100

    bugfix #73

--- spine-web/apache.php
+++ spine-web/apache.php
@@ -257,7 +257,7 @@
       $q = $dbh->prepare("DELETE FROM www_users WHERE id = ".$_GET['rmuser']);
       $q->execute();
 
-      updateConfigVersion($dbh, $_POST['serverid']);
+      updateConfigVersion($dbh, $_POST['serverid'], "apache");
     }
   }
   if(isset($_GET['chpass'])) {
@@ -267,7 +267,7 @@
     $q = $dbh->prepare("UPDATE www_users SET password = '".$pass."' WHERE id = ".$_POST['id']);
     $q->execute();
 
-    updateConfigVersion($dbh, $_POST['serverid']);
+    updateConfigVersion($dbh, $_POST['serverid'], "apache");
   }
   if(isset($_GET['vhid'])) {
     $q = $dbh->prepare("SELECT ServerName FROM www WHERE id = ". $_GET['vhid']);
@@ -282,6 +282,6 @@
     $q = $dbh->prepare("UPDATE www SET status = 'D', purgedir = '".$_POST['rmdir']."' WHERE id = ".$_GET['vhdel']);
     $q->execute();
 
-    updateConfigVersion($dbh, $_POST['serverid']);
+    updateConfigVersion($dbh, $_POST['serverid'], "apache");
   }
 ?>
