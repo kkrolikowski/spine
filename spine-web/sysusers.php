ff454f626410d29f1b288e0af06080e366974989
Krzysztof Królikowski <kkrolikowski@gmail.com>
2017-01-17 06:18:37 +0100

    sudo field

--- spine-web/sysusers.php
+++ spine-web/sysusers.php
@@ -28,6 +28,7 @@
       isset($_POST['isActive'])         ? $active = 1 : $active = 0;
       isset($_POST['shell'])            ? $shell  = 1 : $shell  = 0;
       isset($_POST['sshkey_enable'])    ? $usekey = 1 : $usekey = 0;
+      isset($_POST['sudo'])             ? $sudo   = 1 : $sudo   = 0;
 
       if (isset($_POST['expEnable'])) {
         $timeArr = explode("/", $_POST['expdate']);
@@ -41,10 +42,10 @@
       setlocale(LC_ALL, 'pl_PL.utf8');
       $gecos = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $_POST['fullname']);
 
-      $q = $dbh->prepare("INSERT INTO sysusers(login,pass,fullname,email,system_id,gecos,uid,gid,active,expiration, shell, sshkeys, status) ".
+      $q = $dbh->prepare("INSERT INTO sysusers(login,pass,fullname,email,system_id,gecos,uid,gid,active,expiration, shell, sshkeys, status, sudo) ".
                           "VALUES('".$_POST['login']."', '".$sha512."', '".$_POST['fullname'].
                           "', '".$_POST['email']."', ".$_POST['serverid'].", '".$gecos."', ".$uid.", ".$uid.
-                          ", ".$active.", '".$exptime."', ".$shell.", ".$usekey.", 'N')");
+                          ", ".$active.", '".$exptime."', ".$shell.", ".$usekey.", 'N', ".$sudo.")");
       $q->execute();
 
       $q = $dbh->prepare("SELECT id,login,fullname,email FROM sysusers WHERE login = '".$_POST['login']."' AND system_id = ".$_POST['serverid']);
