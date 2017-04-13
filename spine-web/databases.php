b4644b4a1627592235735ca9d9ca895866fb187d
Krzysztof Królikowski <kkrolikowski@gmail.com>
2017-04-13 18:25:53 +0200

    put and get data from db

--- spine-web/databases.php
+++ spine-web/databases.php
@@ -3,7 +3,22 @@
   include_once './include/functions.php';
   $dbh = DBconnect();
 
-  if(defined($_GET['add'])) {
-    echo $_POST;
+  if(isset($_GET['add'])) {
+    $q = $dbh->prepare("INSERT INTO db_name(name, status, host_id, vhost_id) ".
+                      "VALUES('".$_POST['dbname']."', 'N', ".$_POST['serverid'].
+                      ", ".$_POST['vhostid'].")");
+    $q->execute();
+
+    $q = $dbh->prepare("SELECT d.id, d.name AS dbname, v.ServerName AS vhost FROM db_name d ".
+                      "JOIN www v ON d.vhost_id = v.id WHERE d.host_id = ".$_POST['serverid']);
+    $q->execute();
+    while ($r = $q->fetch()) {
+      $json[$r['id']] = array(
+        'dbname' => $r['dbname'],
+        'vhost' => $r['vhost']
+      );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($json);
   }
 ?>
