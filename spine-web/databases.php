6344ef8dbad3a1c46b02f7656a2a2920c6f6cc49
Krzysztof Królikowski <kkrolikowski@gmail.com>
2017-04-14 00:54:43 +0200

    get only latest record

--- spine-web/databases.php
+++ spine-web/databases.php
@@ -9,8 +9,13 @@
                       ", ".$_POST['vhostid'].")");
     $q->execute();
 
+    $q = $dbh->prepare("SELECT id FROM db_name WHERE name = '".$_POST['dbname']."'");
+    $q->execute();
+    $r = $q->fetch();
+    $dbid = $r['id'];
+
     $q = $dbh->prepare("SELECT d.id, d.name AS dbname, v.ServerName AS vhost FROM db_name d ".
-                      "JOIN www v ON d.vhost_id = v.id WHERE d.host_id = ".$_POST['serverid']);
+                      "JOIN www v ON d.vhost_id = v.id WHERE d.id = ".$dbid);
     $q->execute();
     while ($r = $q->fetch()) {
       $json[$r['id']] = array(
